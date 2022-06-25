5bb70f4ee421cfe1c3c9f8f88d06a506f06d7c35
Davi Gomes <davidarquivo062@gmail.com>
2022-06-24 23:23:18 -0300

    Primaira Página

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import { useState } from "react"
+function Home(){
+   return (
+      <div>
+         <h1>Home</h1>
+         <Contador />
+      </div>
+
+   ) 
+}
+
+function Contador(){
+   const [contador,setContador] = useState(1)
+  
+  function adicionarContador(){
+   setContador(contador + 1)
+  }
+   return(
+      <div>
+         <div>{contador}</div>
+         <p></p>
+         <button onClick={adicionarContador}>Adicionar</button>
+      </div>
+   )
+}
+
+export default Home
